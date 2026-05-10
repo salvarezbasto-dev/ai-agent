38039f3d04a52674470c1fa4af6c81d0d0f289cb
salvarezbasto-dev <s.alvarezbasto@gmail.com>
2026-05-10 00:03:50 -0500

    code now prints remaining prompt and response tokens

--- main.py
+++ main.py
@@ -14,11 +14,8 @@ client = genai.Client(api_key=api_key)
 response = client.models.generate_content(
     model='gemini-2.5-flash', contents="Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum."
 )
-print(response.text)
 
-"""def main():
-    print("Hello from ai-agent!")
+if response.usage_metadata is None:
+    raise RuntimeError("Cannot run request. Metadata is None")
 
-
-if __name__ == "__main__":
-    main()"""
+print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}\nResponse tokens: {response.usage_metadata.candidates_token_count}\nResponse: {response.text}")
