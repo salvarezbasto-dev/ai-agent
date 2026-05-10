8d390a927ee5b7fe41ed99b0648ed776499ccb15
salvarezbasto-dev <s.alvarezbasto@gmail.com>
2026-05-09 23:44:16 -0500

    started ai agent project

---
+++ main.py
@@ -0,0 +1,24 @@
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+api_key = os.environ.get("GEMINI_API_KEY")
+
+if api_key == None:
+    raise RuntimeError("API key not found")
+
+from google import genai
+
+client = genai.Client(api_key=api_key)
+
+response = client.models.generate_content(
+    model='gemini-2.5-flash', contents="Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum."
+)
+print(response.text)
+
+"""def main():
+    print("Hello from ai-agent!")
+
+
+if __name__ == "__main__":
+    main()"""
