3854e2b85cac22e0c96433f21bce4935a3a61356
salvarezbasto-dev <s.alvarezbasto@gmail.com>
2026-05-10 00:14:36 -0500

    imported argparse so user could generate prompts

--- main.py
+++ main.py
@@ -2,18 +2,24 @@ import os
 from dotenv import load_dotenv
 
 load_dotenv()
-api_key = os.environ.get("GEMINI_API_KEY")
+api_key = os.environ.get("GEMINI_API_KEY") # Import API Key to access Google Gemini model
 
 if api_key == None:
     raise RuntimeError("API key not found")
 
-from google import genai
+import argparse # Import argparse for user-generated prompts
+
+parser = argparse.ArgumentParser(description="Chatbot")
+parser.add_argument("user_prompt", type=str, help="User prompt")
+args = parser.parse_args()
+
+from google import genai # Import Google Gemini
 
 client = genai.Client(api_key=api_key)
 
 response = client.models.generate_content(
-    model='gemini-2.5-flash', contents="Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum."
-)
+    model='gemini-2.5-flash', contents=args.user_prompt
+) # Set Gemini 2.5 Flash to generate text outputs out of user-generated text input
 
 if response.usage_metadata is None:
     raise RuntimeError("Cannot run request. Metadata is None")
